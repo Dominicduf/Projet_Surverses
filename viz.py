e216e3dfa2a9be66e31a129dbba9c56422009e69
D-LL <daniel.lussierlevesque@gmail.com>
2021-04-21 17:01:02 -0400

    colormap + allignment fix

--- viz.py
+++ viz.py
@@ -21,13 +21,13 @@ def map(df, mode_size, mode_color):
     # TODO : Update the template to include our new theme and set the title
     if (mode_size =='Durée de déversement') & (mode_color =='cause'):
         fig = px.scatter_mapbox(df, lat='Latitude de l\'émissaire', lon='Longitude de l\'émissaire', size='Durée de débordement (minutes)', color='Contexte du débordement max', hover_name="Nom de la station d'épuration",
-                   size_max=25, zoom=3.75, mapbox_style='open-street-map', center=dict(lat=53 , lon =-74), custom_data=df)
+                   size_max=25, zoom=3.75, mapbox_style='open-street-map', center=dict(lat=53 , lon =-74), custom_data=df, color_discrete_sequence=px.colors.qualitative.Alphabet)
     elif (mode_size =='Durée de déversement') & (mode_color =='age'):
         fig = px.scatter_mapbox(df, lat='Latitude de l\'émissaire', lon='Longitude de l\'émissaire', size='Durée de débordement (minutes)', color='Âge', hover_name="Nom de la station d'épuration",
                    size_max=25, zoom=3.75, mapbox_style='open-street-map', center=dict(lat=53 , lon =-74), custom_data=df)
     elif (mode_size =='Fréquence de déversements') & (mode_color =='cause'):
         fig = px.scatter_mapbox(df, lat='Latitude de l\'émissaire', lon='Longitude de l\'émissaire', size='Fréquence', color ='Contexte du débordement max', hover_name="Nom de la station d'épuration",
-                   size_max=25, zoom=3.75, mapbox_style='open-street-map', center=dict(lat=53 , lon =-74), custom_data=df)
+                   size_max=25, zoom=3.75, mapbox_style='open-street-map', center=dict(lat=53 , lon =-74), custom_data=df, color_discrete_sequence=px.colors.qualitative.Alphabet)
     elif (mode_size =='Fréquence de déversements') & (mode_color =='age'):
         fig = px.scatter_mapbox(df, lat='Latitude de l\'émissaire', lon='Longitude de l\'émissaire', size='Fréquence', color ='Âge', hover_name="Nom de la station d'épuration", hovertemplate=hover_template.map_hover_template(),
                    size_max=25, zoom=3.75, mapbox_style='open-street-map', center=dict(lat=53 , lon =-74), custom_data=df)  
@@ -46,14 +46,14 @@ def line_chart(df, mode, name):
         titre = "Évolution des causes de déversement par année de la<br>" + name
 
     if mode =='Durée de déversement':
-        fig = px.line(df, x='Année', y='Durée de débordement (minutes)', color='Contexte du débordement', title=titre)
+        fig = px.line(df, x='Année', y='Durée de débordement (minutes)', color='Contexte du débordement', title=titre, color_discrete_sequence=px.colors.qualitative.Alphabet)
     elif mode =='Fréquence de déversements':
-        fig = px.line(df, x='Année', y='Fréquence', color='Contexte du débordement', title=titre)
+        fig = px.line(df, x='Année', y='Fréquence', color='Contexte du débordement', title=titre, color_discrete_sequence=px.colors.qualitative.Alphabet)
     fig.update_layout(legend=dict(
         orientation="h",
         yanchor="top",
         xanchor="left",
-        y=-0.15
+        y=-0.15,
     ))
     fig.layout.paper_bgcolor ="rgb(209, 222, 224)"
     fig.update_layout(showlegend=False,legend_title_side='top')
